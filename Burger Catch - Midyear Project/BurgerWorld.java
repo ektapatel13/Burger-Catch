fb419060ba29c353601f1cd53819b5873461a50d
ektapatel13 <ektap713@gmail.com>
2025-01-07 12:11:55 -0500

    testing for christine

--- Burger Catch - Midyear Project/BurgerWorld.java
+++ Burger Catch - Midyear Project/BurgerWorld.java
@@ -7,6 +7,12 @@ public class BurgerWorld extends World
     {    
         // Create a new world with 600x400 cells and 1x1 pixel each
         super(600, 400, 1);
-        //prepare();
+        prepare();
+    }
+    
+    public void prepare() {
+        Platee plate = new Platee();
+        addObject(plate, 100, 100);
+        plate.scale(plate.getWidth()*10/100, plate.getHeight()*10/100);
     }
 }
--- Burger Catch - Midyear Project/Platee.java
+++ Burger Catch - Midyear Project/Platee.java
@@ -2,7 +2,17 @@ import greenfoot.*;
 
 public class Platee extends Actor
 {
+    public void act() {
+        
+    }
     
-
+    public void moveKeys() {
+        if (Greenfoot.isKeyDown("left")) {
+            setLocation(getX()-2, getY());
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            setLocation(getX()+2, getY());
+        }
+    }
     
 }
