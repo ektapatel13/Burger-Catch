339387f58ea3248b3fa11ef3385ee4af1d57fe73
ektapatel13 <ektap713@gmail.com>
2025-01-07 12:01:15 -0500

    deleted everything :

--- Burger Catch - Midyear Project/Platee.java
+++ Burger Catch - Midyear Project/Platee.java
@@ -1,54 +1,8 @@
 import greenfoot.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Platee extends Actor
 {
-    private int speed = 5; 
-
-
-    public Platee()
-    {
-        
-        
     
-    }
-
-    public void act()
-    {
-        movePlate();
-        //checkForCaughtItems();
-    }
-
-    
-    private void movePlate()
-    {
-        if (Greenfoot.isKeyDown("left"))
-        {
-            setLocation(getX() - speed, getY());
-        }
-        if (Greenfoot.isKeyDown("right"))
-        {
-            setLocation(getX() + speed, getY());
-        }
-
-        
-        if (getX() < 0)
-        {
-            setLocation(0, getY());
-        }
-        
-        if (getX() > getWorld().getWidth())
-        {
-            setLocation(getWorld().getWidth(), getY());
-        }
-    }
-
-    
-    private void checkForCaughtItems()
-    {
-        
-    }
 
     
 }
