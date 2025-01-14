f792430a3da9d056ffb63f35d2c8752d3bd19a08
Christineo4321 <140741027+Christineo4321@users.noreply.github.com>
2025-01-14 12:09:01 -0500

    for ekta

--- Burger Catch - Midyear Project/BurgerTop.java
+++ Burger Catch - Midyear Project/BurgerTop.java
@@ -7,10 +7,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class BurgerTop extends IngredientBase
+public class BurgerTop extends Actor 
 {
-    protected int speed = Greenfoot.getRandomNumber(3) + 2;
-    protected boolean didScore = false;
+    private int speed = Greenfoot.getRandomNumber(3) + 2;
+    private boolean didScore = false;
     
     
     /**
